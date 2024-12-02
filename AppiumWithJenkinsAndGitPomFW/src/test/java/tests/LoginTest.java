040a70c4b0747c91041fb561fa093ada06979086
MookappaTN <mookappat@gmail.com>
2024-12-02 21:22:49 +0530

    new changes

--- AppiumWithJenkinsAndGitPomFW/src/test/java/tests/LoginTest.java
+++ AppiumWithJenkinsAndGitPomFW/src/test/java/tests/LoginTest.java
@@ -2,20 +2,21 @@ package tests;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
+
 			
 public class LoginTest extends BaseClass {
 	
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	WebDriverWait wait = new WebDriverWait(BaseClass.getDriver(), Duration.ofSeconds(20));
 	@Test
 	public void verifyLogin() throws InterruptedException {
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText"))).sendKeys("ese04");
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[@resource-id='btnContinueFirst']"))).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath("//android.widget.EditText"))).sendKeys("ese04");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath("//android.widget.Button[@resource-id='btnContinueFirst']"))).click();
 	}
 
 }
